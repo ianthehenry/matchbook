70095e72f45331e972913630d679f7f291e61842
Ian Henry <ianthehenry@gmail.com>
2015-04-07 19:49:50 -0400

    don't use generators
    To avoid depending on the regenerator runtime.

--- src/compile.js
+++ src/compile.js
@@ -3,10 +3,12 @@
 let all = require('./all');
 let PredicatePattern = require('./predicate-pattern');
 
-let zipSameLength = function*(list1, list2) {
+let zipSameLength = (list1, list2) => {
+  const result = [];
   for (let i = 0; i < list1.length; i++) {
-    yield [list1[i], list2[i]];
+    result.push([list1[i], list2[i]]);
   }
+  return result;
 };
 
 let call = ([f, x]) => f(x);
